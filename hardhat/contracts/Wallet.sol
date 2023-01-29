14ec94ba8c059780d1e9c73475d44f9226ab23d6
Ali Kim <alikim@gmail.com>
2023-01-29 17:35:44 -0300

    works with solidity 0.7.6

--- hardhat/contracts/Wallet.sol
+++ hardhat/contracts/Wallet.sol
@@ -1,25 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.12;
+pragma solidity ^0.7.6;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ILendingPool} from "@aave/protocol-v2/contracts/interfaces/ILendingPool.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ILendingPool} from "./interfaces/aave/ILendingPool.sol";
+
+// import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+// import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
 contract Wallet {
     address public owner;
-    mapping(address => uint256) public balances;
-    address public aaveLendingPoolAddr;
+    // mapping(address => uint256) public balances;
+
+    address public constant aaveLendingPoolAddr =
+        0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf;
 
     constructor() public {
         owner = msg.sender;
-        aaveLendingPoolAddr = 0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf;
+        // aaveLendingPoolAddr = 0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf;
     }
 
+    // deposit and withdraw ETH
     function deposit() public payable {
         require(msg.value > 0, "Cannot deposit 0 or less wei");
         require(msg.sender == owner, "Only the owner can deposit funds");
 
         // Depositing funds is the same as transferring them to the contract
-        ERC20(msg.sender).transfer(address(this), msg.value);
+        IERC20(msg.sender).transfer(address(this), msg.value);
     }
 
     function withdraw(uint256 amount) public {
@@ -27,9 +33,10 @@ contract Wallet {
         require(msg.sender == owner, "Only the owner can withdraw funds");
 
         // Withdrawing funds is the same as transferring them from the contract to the owner
-        ERC20(msg.sender).transfer(owner, amount);
+        IERC20(msg.sender).transfer(owner, amount);
     }
 
+    //transfer any ERC20 token
     function transfer(
         address token,
         address recipient,
@@ -39,20 +46,22 @@ contract Wallet {
         require(msg.sender == owner, "Only the owner can transfer tokens");
 
         // Transferring tokens is done by calling the transfer function on the token contract
-        ERC20(token).transfer(recipient, amount);
+        IERC20(token).transfer(recipient, amount);
     }
 
+    //check balance of ERC20 token
     function balanceOf(address token) public view returns (uint256) {
         // The balance is determined by calling the balanceOf function on the token contract
-        return ERC20(token).balanceOf(address(this));
+        return IERC20(token).balanceOf(address(this));
     }
 
+    // AAVE ------------------------------------------------------------
     // aave: deposit funds
     function aave_deposit(address assetAddress, uint256 amount) public {
         // lending pooL: 0x8dff5e27ea6b7ac08ebfdf9eb090f32ee9a30fcf
 
         //approve
-        ERC20(assetAddress).approve(aaveLendingPoolAddr, amount);
+        IERC20(assetAddress).approve(aaveLendingPoolAddr, amount);
 
         //deposit
         ILendingPool(aaveLendingPoolAddr).deposit(
@@ -70,4 +79,6 @@ contract Wallet {
             address(this)
         );
     }
+
+    //Uniswap -----------------
 }
--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,7 +3,34 @@ require("dotenv").config({ path: ".env" })
 
 
 module.exports = {
-    solidity: "0.6.12",
+    solidity: {
+        compilers: [
+            {
+                version: "0.7.6",
+            },
+            {
+                version: "0.6.12",
+            },
+        ],
+        // overrides: {
+        //     "@aave/protocol-v2/contracts/interfaces/ILendingPool.sol": {
+        //         version: "0.6.12",
+        //         settings: {}
+        //     },
+        //     "@aave/protocol-v2/contracts/protocol/libraries/types/DataTypes.sol": {
+        //         version: "0.6.12",
+        //         settings: {}
+        //     },
+        //     "@aave/protocol-v2/contracts/interfaces/ILendingPoolAddressesProvider.sol": {
+        //         version: "0.6.12",
+        //         settings: {}
+        //     },
+        //     "@openzeppelin/contracts/token/ERC20/IERC20.sol": {
+        //         version: "0.6.0",
+        //         settings: {}
+        //     }
+        // }
+    },
     networks: {
         goerli: {
             url: process.env.ALCHEMY_URL,
@@ -12,10 +39,6 @@ module.exports = {
         mumbai: {
             url: process.env.POLYGON_MUMBAI_URL,
             accounts: [process.env.PRIVATE_KEY]
-        },
-        polygonFork: {
-            url: "http://localhost:8545",
-            accounts: ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80']
         },
         hardhat: {
             forking: {
